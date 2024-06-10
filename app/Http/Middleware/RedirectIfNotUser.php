cb3990ef0e8950260d2cf3bd54c89e82e4d02416
Abdul De Borja <86055842+AbdulDeBorja09@users.noreply.github.com>
2024-06-10 18:20:05 +0800

    Update route

--- app/Http/Middleware/RedirectIfNotUser.php
+++ app/Http/Middleware/RedirectIfNotUser.php
@@ -5,7 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-class RedirectIfNotAdmin
+
+class RedirectIfNotUser
 {
     /**
      * Handle an incoming request.
@@ -16,8 +17,8 @@ class RedirectIfNotAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->user_type !== 'admin') {
-            return redirect('/');
+        if (Auth::check() && Auth::user()->user_type !== 'user') {
+            return redirect('/admin/home');
         }
 
         return $next($request);
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'user'])->group(function () {
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/profile', [App\Http\Controllers\HomeController::class, 'profile'])->name('user.profile');
     Route::get('/transaction', [App\Http\Controllers\HomeController::class, 'transaction'])->name('user.transaction');
@@ -15,7 +15,6 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home');
     Route::get('/admin/tenants', [App\Http\Controllers\AdminController::class, 'tenants'])->name('admin.tenants');
     Route::get('/admin/tenants/add', [App\Http\Controllers\AdminController::class, 'Addtenant'])->name('admin.Addtenant');
-
 
     Route::post('/admin/tenants/add', [App\Http\Controllers\AdminController::class, 'SubmitTenant'])->name('admin.Addtenant');
 });
